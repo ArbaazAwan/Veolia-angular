cac9d2deb956668b83b52fd4b9ba009145727223
Arbaaz Awan <awanarbaaz@gmail.com>
2022-09-05 16:37:19 +0500

    added login/registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'login', component:LoginComponent },
+      { path: 'registration', component:RegistrationComponent },
+      { path: 'dashboard',component:DashboardComponent },
+      { path: '', redirectTo:'dashboard', pathMatch:'full' },
+      { path: '**', redirectTo:'dashboard', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
