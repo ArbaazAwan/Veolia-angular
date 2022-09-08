9d6baa2b28561195e221f180bc06c976abca53dc
ArbaazAwan <48342373+ArbaazAwan@users.noreply.github.com>
2022-09-08 19:41:09 +0500

    github pages deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,32 +5,35 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegistrationComponent } from './auth/registration/registration.component';
 import { RouterModule } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { FormsModule } from '@angular/forms';
-import { ModalFormComponent } from './modal-form/modal-form.component';
 
 import { AuthModule } from './auth/auth.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { Error404Component } from './errors/404.component';
+import { ModelTableComponent } from './model-table/model-table.component';
+import { ModelTableModule } from './model-table/model-table.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    Error404Component,
   ],
   imports: [
     AuthModule,
     DashboardModule,
+    ModelTableModule,
 
     BrowserModule,
     RouterModule.forRoot([
       { path: 'login', component:LoginComponent },
       { path: 'registration', component:RegistrationComponent },
       { path: 'dashboard', component:DashboardComponent },
-      { path: 'modal', component:ModalFormComponent},
+      { path: 'modeltable', component: ModelTableComponent },
       { path: '', redirectTo:'login', pathMatch:'full' },
-      { path: '**', redirectTo:'login', pathMatch: 'full' }
+      { path: '**', component: Error404Component }
     ]),
     NgbModule,
     FormsModule
